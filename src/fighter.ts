608dc51e975ed90e8ee87befc01a0692c5c12d94
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-01-31 19:33:53 +0100

    Added specific type of characters

---
+++ src/fighter.ts
@@ -0,0 +1,16 @@
+/* eslint-disable max-params */
+/* eslint-disable no-unused-vars */
+import { Character } from './character';
+
+export class Fighter extends Character {
+  constructor(
+    name: string,
+    family: string,
+    age: number,
+    public weapon: string,
+    public skill: number
+  ) {
+    super(name, family, age, 'fighter');
+    this.message = 'Primero pego y luego pregunto';
+  }
+}
