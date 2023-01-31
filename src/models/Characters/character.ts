3625f384cd568a0c8985d4a9767935055f7c519d
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-01-31 20:11:51 +0100

    Added characters importing classes

---
+++ src/models/Characters/character.ts
@@ -0,0 +1,10 @@
+import { King } from '../../king';
+import { Fighter } from '../../fighter';
+import { Counselor } from '../../counselor';
+import { Squire } from '../../squire';
+
+const jeoffrey = new King('Jeoffrey', 'Baratheon', 12, 2);
+const jamie = new Fighter('Jamie', 'Lannister', 31, 'big sword', 9);
+const danaerys = new Fighter('Danaerys', 'Targaryen', 19, 'dracarys', 2);
+const tyrion = new Counselor('Tyrion', 'Lannister', 26, danaerys);
+const bronn = new Squire('Bronn', 'from the dark waters', 37, 4, jamie);
